1911bf3624dfcb1a523a85bb0beb0b301506c54d
aravind <aravindar433@gmail.com>
2026-01-27 15:59:12 +0530

    add db schema and seed test text posts

--- app/app.py
+++ app/app.py
@@ -1,12 +1,28 @@
 from fastapi import FastAPI, HTTPException
+from app.schemas import PostCreate
+from app.db import Post, create_db_and_tables, get_async_session
+from sqlalchemy.ext.asyncio import AsyncSession
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await create_db_and_tables()
+    yield
+    
+app = FastAPI(lifespan=lifespan)
 
-test_posts = {1: {"title": "new_post", 'content': 'why am i doing this?'},
-        2: {"title": "new_post", 'content': '2why am i doing this?'},
-        3: {"title": "new_post", 'content': '3why am i doing this?'},
-        4: {"title": "new_post", 'content': '4why am i doing this?'},
-        5: {"title": "new_post", 'content': '5why am i doing this?'}}
+text_posts = {
+    1: {"title": "midnight_thoughts", "content": "Why does everything feel heavier at night?"},
+    2: {"title": "small_wins", "content": "Progress is quiet, but it still counts."},
+    3: {"title": "burnout_mode", "content": "I’m tired, but I’m not done."},
+    4: {"title": "overthinking", "content": "My brain won’t stop running marathons."},
+    5: {"title": "reset_day", "content": "Starting over is still moving forward."},
+    6: {"title": "focus_check", "content": "Discipline beats motivation every time."},
+    7: {"title": "self_talk", "content": "I need to be kinder to myself."},
+    8: {"title": "future_me", "content": "I hope I’m proud of this version of me."},
+    9: {"title": "grind_hours", "content": "No one sees the work behind the scenes."},
+    10: {"title": "calm_before", "content": "Peace is learning to pause."}
+}
 
 @app.get("/")
 def root():
@@ -19,11 +35,17 @@ def hello_world():
 @app.get('/post')
 def get_post(limit:int):
     if limit:
-        return list(test_posts.values())[:limit]
-    return test_posts
+        return list(text_posts.values())[:limit]
+    return text_posts
 
 @app.get('/post/{id}')
 def get_post(id:int):
-    if id not in test_posts:
+    if id not in text_posts:
         raise HTTPException(status_code= 404, detail='post not found')
-    return test_posts.get(id)
+    return text_posts.get(id)
+
+@app.post("/posts")
+def create_post(post:PostCreate):
+    new_post = {'title':post.title, "content": post.content}
+    text_posts[max(text_posts.keys()) + 1] = new_post
+    return new_post
