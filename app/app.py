f26da91af15b4e98f97282f9074066a55d59f188
aravind <aravindar433@gmail.com>
2026-01-29 10:32:16 +0530

    added upload and feed methods to the app.py file

--- app/app.py
+++ app/app.py
@@ -1,8 +1,13 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, File, UploadFile, Form, Depends
 from app.schemas import PostCreate
 from app.db import Post, create_db_and_tables, get_async_session
 from sqlalchemy.ext.asyncio import AsyncSession
 from contextlib import asynccontextmanager
+from sqlalchemy import select
+from app.images import imageKit
+from imagekitio.models import UploadFileRequestOptions
+from imagekitio import models
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -11,41 +16,40 @@ async def lifespan(app: FastAPI):
     
 app = FastAPI(lifespan=lifespan)
 
-text_posts = {
-    1: {"title": "midnight_thoughts", "content": "Why does everything feel heavier at night?"},
-    2: {"title": "small_wins", "content": "Progress is quiet, but it still counts."},
-    3: {"title": "burnout_mode", "content": "I’m tired, but I’m not done."},
-    4: {"title": "overthinking", "content": "My brain won’t stop running marathons."},
-    5: {"title": "reset_day", "content": "Starting over is still moving forward."},
-    6: {"title": "focus_check", "content": "Discipline beats motivation every time."},
-    7: {"title": "self_talk", "content": "I need to be kinder to myself."},
-    8: {"title": "future_me", "content": "I hope I’m proud of this version of me."},
-    9: {"title": "grind_hours", "content": "No one sees the work behind the scenes."},
-    10: {"title": "calm_before", "content": "Peace is learning to pause."}
-}
+@app.post('/upload')
+async def upload_file(
+    file: UploadFile = File(...),
+    caption: str = Form(''),
+    session: AsyncSession = Depends(get_async_session)
+):
+    post = Post(
+        caption = caption,
+        url = "dummy url",
+        file_type = "phot",
+        file_names = "dummy name"
+    )
+    session.add(post)
+    await session.commit()
+    await session.refresh(post)
+    return post
 
-@app.get("/")
-def root():
-    return {"status": "API is running"}
+@app.get("/feed")
+async def get_feed(
+        session: AsyncSession = Depends(get_async_session)
+):
+    result = await session.execute(select(Post).order_by(Post.created_at.desc()))
+    posts = [row[0] for row in result.all()]
 
-@app.get("/hello-world")
-def hello_world():
-    return {"message": "Hello World"}
-
-@app.get('/post')
-def get_post(limit:int):
-    if limit:
-        return list(text_posts.values())[:limit]
-    return text_posts
-
-@app.get('/post/{id}')
-def get_post(id:int):
-    if id not in text_posts:
-        raise HTTPException(status_code= 404, detail='post not found')
-    return text_posts.get(id)
-
-@app.post("/posts")
-def create_post(post:PostCreate):
-    new_post = {'title':post.title, "content": post.content}
-    text_posts[max(text_posts.keys()) + 1] = new_post
-    return new_post
+    posts_data = []
+    for post in posts:
+        posts_data.append(
+        {
+            "id": str(post.id),
+            "caption": post.caption,
+            "url": post.url,
+            "file_name" : post.file_name,
+            "created_at": post.created_at.isoformat()
+        }
+        )
+    return {"posts": posts_data}
+    
